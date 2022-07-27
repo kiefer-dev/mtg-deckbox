c17940d471da5a04f6a769b917b5d03ec2167f1c
Kiefer Weis <kieferweis@protonmail.com>
2022-07-27 19:32:04 -0400

    Add getCard function to search for cards by name

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-document.querySelector("buttonCardSearch").addEventListener('click', getCard);
+document.getElementById('buttonCardSearch').addEventListener('click', getCard);
 
-const mtg = require('mtgsdk');
+// const mtg = require('mtgsdk');
 
 function getCard() {
+  const cardNameInput = document.getElementById('inputCardName').value.split(" ").join("%20");
+  const url = `https://api.magicthegathering.io/v1/cards?name=${cardNameInput}`
 
+  fetch(url)
+    .then(res => res.json()) //parse response as JSON
+    .then(data => {
+      console.log(cardNameInput);
+      console.log(data);
+
+      const imageUrls = [];
+      data.cards.forEach(obj => {
+        if (obj.imageUrl) {
+          imageUrls.push(obj.imageUrl);
+        }
+      })
+
+
+
+      console.log(imageUrls);
+    })
 }
