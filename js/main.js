07f8b0543fa8d9d530be9d6bd64cb78e445c46b5
Kiefer Weis <kieferweis@protonmail.com>
2022-07-28 16:03:24 -0400

    Simplify the fetch's cmc filter functionality

--- js/main.js
+++ js/main.js
@@ -7,39 +7,52 @@ let resultArray = []; //to hold card results
 function getCard() {
   // Take whether the search term is for card name or card subtype
   const nameOrSubtype = document.getElementById('selectNameOrSubtype').value;
+
   // Take the input search term and sanitize it for API use
   const inputSearch = document.getElementById('inputCardSearch').value.split(" ").join("%20");
+
   // Create the fetch url to search for cards with the supplied name/subtype and that contains a card image.
   const url = `https://api.magicthegathering.io/v1/cards?${nameOrSubtype}=${inputSearch}&contains=imageUrl`;
+
   // Store the Converted Mana Cost selection
   const cmcSelection = document.getElementById('selectCMC').value;
+
   // Clear the results list on button click
   document.querySelector('#cardImages').innerHTML = '';
+
   // Clear the results count on button click
   resultCount = 0;
 
+  // API fetch
   fetch(url)
     .then(res => res.json()) //parse response as JSON
     .then(data => {
-      // Store all found cards with CMC 10+ into resultArray
-      if (cmcSelection === '10+') {
-        resultArray = data.cards.filter(card => card.cmc >= 10) 
-      } else {
-        // Store all found cards with images and selected CMC into resultArray
-        cmcSelection
-          ? resultArray = data.cards.filter(card => card.cmc == cmcSelection)
-          : resultArray = data.cards;
+      // Store cards into resultArray
+      if (cmcSelection) { //if a converted mana cost has been selected
+        cmcSelection === '10+'
+          ? resultArray = data.cards.filter(card => card.cmc >= 10) //if 10+
+          : resultArray = data.cards.filter(card => card.cmc == cmcSelection) //if 0-9
+      } else { //if a converted mana cost has not been selected
+        resultArray = data.cards;
       }
 
       console.log(inputSearch);
       console.log(resultArray);
 
       resultArray.forEach(card => {
-        const img = document.createElement('img'); //create image element to add to DOM
-        img.src = card.imageUrl; //set img's src to the current card's imageUrl
-        document.querySelector('#cardImages').appendChild(img); //add the card image to the DOM
-        resultCount++; //increment count of results for each valid card found
+        // Create image element to add to DOM
+        const img = document.createElement('img'); 
+
+        // Set img's src to the current card's imageUrl
+        img.src = card.imageUrl; 
+
+        // Add the card image to the DOM in the #cardImages section
+        document.querySelector('#cardImages').appendChild(img); 
+
+        // Increment count of results for each valid card found
+        resultCount++; 
       })
+      
       // Put amount of results into the DOM
       document.getElementById('numberOfResults').innerText = `${resultCount} results found`;
 
