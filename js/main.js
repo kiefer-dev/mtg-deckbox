43bbb780e7e337757513a4ca8682a5b687d7b968
Kiefer Weis <kieferweis@protonmail.com>
2022-07-28 12:37:22 -0400

    Add dropdown and updated search function to search by card name or card subtype

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
+// Click event on button to search for cards
 document.getElementById('buttonCardSearch').addEventListener('click', getCard);
 
-// const mtg = require('mtgsdk');
-
 function getCard() {
-  const cardNameInput = document.getElementById('inputCardName').value.split(" ").join("%20");
-  const url = `https://api.magicthegathering.io/v1/cards?name=${cardNameInput}`
+  // Take whether the search term is for card name or card subtype
+  const nameOrSubtype = document.getElementById('selectNameOrSubtype').value;
+  // Take the input search term and sanitize it for API use
+  const inputSearch = document.getElementById('inputCardSearch').value.split(" ").join("%20");
+  // Create the fetch url
+  const url = `https://api.magicthegathering.io/v1/cards?${nameOrSubtype}=${inputSearch}`;
+  // Clear the results list on button click
+  document.querySelector('#cardImages').innerHTML = '';
 
   fetch(url)
     .then(res => res.json()) //parse response as JSON
     .then(data => {
-      console.log(cardNameInput);
+      console.log(inputSearch);
       console.log(data);
 
-      document.querySelector('#cardImages').innerHTML = '';
+      
 
       data.cards.forEach(obj => {
         if (obj.imageUrl) {
           const img = document.createElement('img');
           img.src = obj.imageUrl;
-          document.querySelector('div').appendChild(img);
+          document.querySelector('#cardImages').appendChild(img);
         }
       })
 
