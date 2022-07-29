cd05476927b60c97103e02b81ebfe23112819e56
Kiefer Weis <kieferweis@protonmail.com>
2022-07-29 16:47:32 -0400

    Add deckID select to add a card. Successfully connect to MongoDB with basic get/post methods to post new cards (deckID, cardID) to the database

--- server.js
+++ server.js
@@ -3,28 +3,53 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
-// Activate body-parser to parse text submitted in the html form
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Create the server that browsers can connect to
-app.listen(PORT, function() {
-  console.log(`listening on ${PORT}`);
-})
-
-//---------------------------------------------------------------
-
-// READ / GET
-// When requesting the root directory, serve the main page (/index.html)
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); // __dirname is the current directory
-})
 
 
+// Connect to MongoDB through its connect method
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = 'mongodb+srv://kiefer:weis@cluster0.yhggoed.mongodb.net/?retryWrites=true&w=majority'
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to mongoDB!');
+    const db = client.db('mtg-deck-saver')
+    const deck1Collection = db.collection('deck1'); //create collection for deck1
+    
+    // Activate body-parser to parse text submitted in the html form
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-// CREATE / POST
-app.post('/deck', (req, res) => {
-  console.log(req.body);
-})
+    // READ / GET
+    // When requesting the root directory, serve the main page (/index.html)
+    app.get('/', (req, res) => {
+      db.collection('deck1').find().toArray()
+        .then(results => {
+          console.log(results)
+        })
+        .catch(error => console.error(error));
+
+      res.sendFile(__dirname + '/index.html'); // __dirname is the current directory
+    })
+
+    // CREATE / POST
+    app.post('/deck', (req, res) => {
+      deck1Collection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/');
+        })
+        .catch(error => console.error(error))
+    })
+
+    // Create the server that browsers can connect to
+    app.listen(PORT, function() {
+      console.log(`listening on ${PORT}`);
+    })
+  })
+  .catch(error => console.error(error));
+
+
+
+
+
+
 
 
 
